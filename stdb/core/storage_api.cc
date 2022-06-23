00de853607c743867b4426c2d7c669980356e190
largestone <huzelin1008@163.com>
2022-06-23 13:22:19 +0800

    add storage api

---
+++ stdb/core/storage_api.cc
@@ -0,0 +1,144 @@
+/*!
+ * \file storage_api.cc
+ */
+#include "stdb/core/storage_api.h"
+
+#include <thread>
+
+#include <boost/exception/all.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "stdb/core/cursor.h"
+#include "stdb/core/storage.h"
+
+namespace stdb {
+
+struct BaseCursor : public DbCursor {
+  std::unique_ptr<ExternalCursor> cursor_;
+  std::string query_;
+
+  BaseCursor(std::shared_ptr<StorageSession> storage_session, const std::string& query) : query_(query) { }
+
+  size_t read(void *dest, size_t dest_size) override {
+    return cursor_->read(dest, dest_size);
+  }
+
+  bool is_done() override {
+    return cursor_->is_done();
+  }
+
+  bool is_error(common::Status *out_error_code_or_null) override {
+    return cursor_->is_error(out_error_code_or_null);
+  }
+
+  bool is_error(const char** error_message, common::Status *out_error_code_or_null) override {
+    return cursor_->is_error(error_message, out_error_code_or_null);
+  }
+
+  void close() override {
+    cursor_->close();
+  }
+};
+
+struct QueryCursor : public BaseCursor {
+  QueryCursor(std::shared_ptr<StorageSession> storage_session, const std::string& query) : BaseCursor(storage_session, query) {
+    cursor_ = ConcurrentCursor::make(&StorageSession::query, storage_session, query_.data());
+  }
+}; 
+
+struct SuggestCursor : public BaseCursor {
+  SuggestCursor(std::shared_ptr<StorageSession> storage_session, const std::string& query) : BaseCursor(storage_session, query) {
+    cursor_ = ConcurrentCursor::make(&StorageSession::suggest, storage_session, query_.data());
+  }
+};
+
+struct SearchCursor : public BaseCursor {
+  SearchCursor(std::shared_ptr<StorageSession> storage_session, const std::string& query) : BaseCursor(storage_session, query) {
+    cursor_ = ConcurrentCursor::make(&StorageSession::search, storage_session, query_.data());
+  }
+};
+
+// STDBSession implementation
+STDBSession::STDBSession(std::shared_ptr<StorageSession> storage_session) : storage_session_(storage_session) { }
+
+STDBSession::~STDBSession() { }
+
+common::Status STDBSession::write(const Sample &sample) {
+  return storage_session_->write(sample);
+}
+
+std::shared_ptr<DbCursor> STDBSession::query(const std::string& query) {
+  std::shared_ptr<DbCursor> ret;
+  ret.reset(new QueryCursor(storage_session_, query));
+  return ret;
+}
+
+std::shared_ptr<DbCursor> STDBSession::suggest(const std::string& query) {
+  std::shared_ptr<DbCursor> ret;
+  ret.reset(new SuggestCursor(storage_session_, query));
+  return ret;
+}
+
+std::shared_ptr<DbCursor> STDBSession::search(const std::string& query) {
+  std::shared_ptr<DbCursor> ret;
+  ret.reset(new SearchCursor(storage_session_, query));
+  return ret;
+}
+
+int STDBSession::param_id_to_series(ParamId id, char *buffer, size_t buffer_size) {
+  return storage_session_->get_series_name(id, buffer, buffer_size);
+}
+
+common::Status STDBSession::series_to_param_id(const char *name, size_t size, Sample *sample) {
+  return storage_session_->init_series_id(name, name + size, sample);
+}
+
+int STDBSession::name_to_param_id_list(const char* begin, const char* end, ParamId* ids, u32 cap) {
+  return storage_session_->get_series_ids(begin, end, ids, cap);
+}
+
+// Connection implementation
+STDBConnection::STDBConnection(const char *path, const FineTuneParams& params) : dbpath_(path) {
+  if (path == std::string(":memory:")) {
+    storage_ = std::make_shared<Storage>();
+  } else {
+    storage_ = std::make_shared<Storage>(path, params);
+    storage_->initialize_input_log(params);
+  }
+}
+
+STDBConnection::~STDBConnection() {
+  LOG(INFO) << "Close database at " << dbpath_;
+  storage_->close();
+}
+
+std::string STDBConnection::get_all_stats() {
+  auto ptree_stats = storage_->get_stats();
+  std::stringstream out;
+  boost::property_tree::json_parser::write_json(out, ptree_stats, true);
+  return out.str();
+}
+
+std::shared_ptr<DbSession> STDBConnection::create_session() {
+  auto disp = storage_->create_write_session();
+  std::shared_ptr<DbSession> result;
+  result.reset(new STDBSession(disp));
+  return result;
+}
+
+common::Status STDBConnection::create_database(
+    const char* base_file_name, const char* metadata_path,
+    const char* volumes_path, i32 num_volumes, bool allocate) {
+  static const u64 vol_size = 4096ull * 1024 * 1024; // pages (4GB total)
+  return create_database_ex(base_file_name, metadata_path, volumes_path, num_volumes, vol_size, allocate);
+}
+
+common::Status STDBConnection::create_database_ex(
+    const char* base_file_name, const char* metadata_path,
+    const char* volumes_path, i32 num_volumes,
+    u64 volume_size, bool allocate) {
+  return Storage::new_database(base_file_name, metadata_path, volumes_path, num_volumes, volume_size, allocate);
+}
+
+}  // namespace stdb
---
+++ stdb/core/storage_api.h
@@ -0,0 +1,126 @@
+/**
+ * \file storage_api.h
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef STDB_CORE_STORAGE_API_H_
+#define STDB_CORE_STORAGE_API_H_
+
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "stdb/common/basic.h"
+#include "stdb/common/status.h"
+
+namespace stdb {
+
+//! Abstraction layer above Cursor
+struct DbCursor {
+  virtual ~DbCursor() = default;
+  //! Read data from cursor
+  virtual size_t read(void* dest, size_t dest_size) = 0;
+
+  //! Check is cursor is done reading
+  virtual bool is_done() = 0;
+
+  //! Check for error condition
+  virtual bool is_error(common::Status* out_error_code_or_null) = 0;
+
+  //! Check for error condition
+  virtual bool is_error(const char** error_message, common::Status* out_error_code) = 0;
+
+  //! Close cursor
+  virtual void close() = 0;
+};
+
+//! Database session, maps to Session directly
+struct DbSession {
+  virtual ~DbSession() = default;
+
+  //! Write value to DB
+  virtual common::Status write(const Sample& sample) = 0;
+
+  //! Execute database query
+  virtual std::shared_ptr<DbCursor> query(const std::string& query) = 0;
+
+  //! Execute suggest query
+  virtual std::shared_ptr<DbCursor> suggest(const std::string& query) = 0;
+
+  //! Execute search query
+  virtual std::shared_ptr<DbCursor> search(const std::string& query) = 0;
+
+  //! Convert paramid to series name
+  virtual int param_id_to_series(ParamId id, char* buffer, size_t buffer_size) = 0;
+
+  virtual common::Status series_to_param_id(const char* name, size_t size, Sample* sample) = 0;
+
+  virtual int name_to_param_id_list(const char* begin, const char* end, ParamId* ids, u32 cap) = 0;
+};
+
+//! Abstraction layer above Database
+struct DbConnection {
+
+  virtual ~DbConnection() = default;
+
+  virtual std::string get_all_stats() = 0;
+
+  virtual std::shared_ptr<DbSession> create_session() = 0;
+};
+
+class StorageSession;
+
+class STDBSession : public DbSession {
+  std::shared_ptr<StorageSession> storage_session_;
+ 
+ public:
+  STDBSession(std::shared_ptr<StorageSession> storage_session);
+  virtual ~STDBSession();
+
+  virtual common::Status write(const Sample &sample) override;
+  virtual std::shared_ptr<DbCursor> query(const std::string& query) override;
+  virtual std::shared_ptr<DbCursor> suggest(const std::string& query) override;
+  virtual std::shared_ptr<DbCursor> search(const std::string& query) override;
+  virtual int param_id_to_series(ParamId id, char *buffer, size_t buffer_size) override;
+  virtual common::Status series_to_param_id(const char *name, size_t size, Sample *sample) override;
+  virtual int name_to_param_id_list(const char* begin, const char* end, ParamId* ids, u32 cap) override;
+};
+
+class Storage;
+
+//! Object of this class writes everything to the database
+class STDBConnection : public DbConnection {
+  std::string   dbpath_;
+  std::shared_ptr<Storage> storage_;
+
+ public:
+  STDBConnection(const char* path, const FineTuneParams &params);
+
+  virtual ~STDBConnection() override;
+
+  virtual std::string get_all_stats() override;
+
+  virtual std::shared_ptr<DbSession> create_session() override;
+
+  static common::Status create_database(const char* base_file_name, const char* metadata_path,
+                                        const char* volumes_path, i32 num_volumes, bool allocate);
+
+  static common::Status create_database_ex(const char* base_file_name, const char* metadata_path,
+                                           const char* volumes_path, i32 num_volumes,
+                                           u64 volume_size, bool allocate);
+};
+
+}  // namespace stdb
+
+#endif  // STDB_CORE_STORAGE_API_H_
